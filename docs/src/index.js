89bf9577c17e354a9241a54f66d5e2f3006af622
Anand Narayan <anand@codebrahma.com>
2018-07-21 01:27:03 -0700

    added docs for testimonial

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import Button from '../../src/Button'
+import Testimonial from '../../src/Testimonial'
 import Playground from 'component-playground'
+import theme from '../../src/theme'
+import {ThemeProvider} from 'styled-components'
 
 export default class extends React.Component {
   render(){
   const button = `
+  <ThemeProvider theme={theme}>
     <Button>
       Serverless
     </Button>
+  </ThemeProvider>
   `;
   const secondary = `
+  <ThemeProvider theme={theme}>
     <Button secondary>
-      Rocks
+      Serverless
     </Button>
+  </ThemeProvider>
   `;
+
+  const testimonial = `
+  <ThemeProvider theme={theme}>
+  <Testimonial 
+      img="https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/icons/cocacola-logo.png"
+      name="Patrick Brandt"
+      designation="Solutions Architect at The Coca Cola Company"
+    >
+    "The Serverless Framework is a core component of The Coca-Cola Company's initiative to reduce IT operational costs and deploy services faster."
+    </Testimonial>
+  </ThemeProvider>
+  `
     return(
       <div className="component-documentation">
-        <Playground codeText={button} scope={{React: React, Button: Button}}/>
-        <Playground codeText={secondary} scope={{React: React, Button: Button}}/>
+          <Playground codeText={button} scope={{React: React, Button: Button, ThemeProvider: ThemeProvider, theme: theme}}/>
+          <Playground codeText={secondary} scope={{React: React, Button: Button, ThemeProvider: ThemeProvider,theme: theme}}/>
+          <Playground codeText={testimonial} scope={{React: React, Testimonial: Testimonial, ThemeProvider: ThemeProvider,theme: theme}}/>
       </div>
     );
   }
--- docs/src/template.js
+++ docs/src/template.js
@@ -11,6 +11,14 @@ module.exports = ({
     font-family: 'serverless';
     src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/serverless.otf') format('opentype');
   }
+  @font-face {
+    font-family: 'SoleilBk';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/soleil-bk.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'Soleil';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/soleil.otf') format('opentype');
+  }
   .playground {
     display: flex;
     align-items:center;
@@ -20,13 +28,13 @@ module.exports = ({
     width: 30%;
   }
   .playgroundPreview {
-    width: 40%;
+    width: 70%;
     margin-left: 20px;
   }
   </style>
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/codemirror.min.css"/>
   <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/codemirror/5.0.0/theme/monokai.min.css"/>
 </head>
-<div id=root>${html}</div>
+<div id=root><ThemeProvider theme={theme}>${html}</ThemeProvider></div>
 ${scripts}
 `
