1753380953895a4fc49fd703149f7f1bedcf8ec9
prasanna1211 <prasanna@codebrahma.com>
2018-07-29 00:15:22 +0530

    removed unncessary files

--- docs/.cache/sync-requires.js
+++ docs/.cache/sync-requires.js
@@ -7,18 +7,12 @@ exports.layouts = {
 }
 
 exports.components = {
-  "component---src-templates-project-js": preferDefault(require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/src/templates/project.js")),
   "component---cache-dev-404-page-js": preferDefault(require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/src/pages/index.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/layout-index.json"),
-  "amy-luo.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/amy-luo.json"),
-  "matt-cannon.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/matt-cannon.json"),
-  "mona-magnussen.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/mona-magnussen.json"),
-  "roman-kraft.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/roman-kraft.json"),
-  "marion-michele.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/marion-michele.json"),
   "dev-404-page.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/dev-404-page.json"),
   "index.json": require("/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/index.json")
 }
