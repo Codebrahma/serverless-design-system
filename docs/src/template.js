5cf7679a5fade1e9cc776ab3d0d3327551bd00af
prasanna1211 <prasanna@codebrahma.com>
2018-07-29 12:29:14 +0530

    added original src components

---
+++ docs/src/template.js
@@ -0,0 +1,31 @@
+// example template
+
+module.exports = ({
+  html,
+  scripts,
+}) => `<!DOCTYPE html>
+<head>
+  <title> Design Components</title>
+  <style>
+  @font-face {
+    font-family: 'serverless';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/serverless.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'SoleilBk';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/soleil-bk.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'Soleil';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/soleil.otf') format('opentype');
+  }
+  @font-face {
+    font-family: 'SoleilLt';
+    src: url('https://s3-us-west-2.amazonaws.com/assets.site.serverless.com/fonts/soleil-lt.otf') format('opentype');
+  }
+  
+  </style>
+</head>
+<div id=root>${html}</div>
+${scripts}
+`
