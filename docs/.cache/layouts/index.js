257842f347977b0b3856c62041dd49d47ec42f46
prasanna1211 <prasanna@codebrahma.com>
2018-07-29 10:46:12 +0530

    removed cache and public

--- docs/.cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/codebrhama/Documents/projects/serverless-design-system/docs/src/layouts/index.js"
-  import data from "/Users/codebrhama/Documents/projects/serverless-design-system/docs/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
