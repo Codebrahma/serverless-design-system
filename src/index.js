5ea8ebff1b63c43e6934379e8a0bef04d14b24c0
prasanna1211 <prasanna@codebrahma.com>
2018-07-30 17:17:57 +0530

    changed component scoping in live edit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as Image } from './Image';
+export { default as Testimonial } from './Testimonial';
+export { default as Text } from './Text';
+export { default as TextField } from './TextField';
