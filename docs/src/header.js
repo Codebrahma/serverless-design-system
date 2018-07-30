6056cfb8265981376901084a789b41be2542020d
prasanna1211 <prasanna@codebrahma.com>
2018-07-31 01:28:59 +0530

    Added header component

---
+++ docs/src/header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LiveEdit from './LiveEdit';
+
+const header = `
+  <Header logo={
+    <Logo
+      src='https://serverless.com/_/src/assets/images/logo.ce91bef30d19103f7a9be878a9b44465.png'
+      height="30px"
+      width="170px"
+      alt="Serverless"
+    />
+  } />
+`;
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div>
+        <LiveEdit code={header} />
+      </div>
+    );
+  }
+}
--- src/Box.js
+++ src/Box.js
@@ -1,14 +1,24 @@
 import styled from 'styled-components';
-import { space, width, color, borderColor,
-  border, display,
-  flexWrap, flexDirection, justifyContent, order }
-  from 'styled-system';
+import {
+  space, width, color,
+  borderColor, border, display,
+  flexWrap, flexDirection,
+  alignItems, justifyContent,
+  order, position, zIndex,
+  top, left, right, bottom,
+  maxWidth,
+} from 'styled-system';
 
 const Box = styled.div`
-  ${space} ${width} ${color} 
-  ${border} ${borderColor}
-  ${display} ${flexWrap} ${flexDirection} 
-  ${justifyContent} ${order}
+  ${space} ${width} ${color}
+  ${border} ${borderColor} ${display}
+  ${flexWrap} ${flexDirection}
+  ${alignItems} ${justifyContent}
+  ${order} ${position} ${zIndex}
+  ${top} ${left} ${right} ${bottom}
+  ${maxWidth}
+
+  background-color: ${(props) => props.backgroundColor}
 `;
 
 Box.displayName = 'Box';
