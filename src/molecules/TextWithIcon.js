d05258fc639bb261335c0171842569926f3e50b2
prasanna1211 <prasanna@codebrahma.com>
2018-07-30 23:59:27 +0530

    Fixed default props

--- src/molecules/TextWithIcon.js
+++ src/molecules/TextWithIcon.js
@@ -1,29 +1,27 @@
 import styled from 'styled-components';
-import { space, width, color, borderColor,
-  border, display,
-  flexWrap, flexDirection, justifyContent, order
-} from 'styled-system';
+import { backgroundSize, position } from 'styled-system';
 
-const TextWithIcon = styled.div`
-  ${space} ${width} ${color}
-  ${border} ${borderColor}
-  ${display} ${flexWrap} ${flexDirection}
-  ${justifyContent} ${order}
+const TextWithIcon = styled.span`
   position: relative;
 
   > h1, h2, h3, h4, h5, h6, span, p, div {
     &:before {
       content: ' ';
-      position: absolute;
       top: ${props => props.iconTop};
       height: ${props => props.iconHeight};
       width: ${props => props.iconWidth};
       background-image: url(${props => props.iconSrc});
-      background-size: cover;
+      ${position}
+      ${backgroundSize}
     }
   }
 `;
 
 TextWithIcon.displayName = 'TextWithIcon';
+
+TextWithIcon.defaultProps = {
+  position: 'absolute',
+  backgroundSize: 'cover',
+};
 
 export default TextWithIcon;
