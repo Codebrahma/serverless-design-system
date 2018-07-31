3846bb08913c9f120ec8a2368039ed73a4eb8b39
prasanna1211 <prasanna@codebrahma.com>
2018-07-31 10:12:10 +0530

    replaced and changed folders for molecules

--- src/atoms/ListItem/index.js
+++ src/atoms/ListItem/index.js
@@ -13,21 +13,6 @@ import {
   width,
 } from 'styled-system';
 
-// lineHeight is inconsistent
-const footer = (props) => {
-  if (props.footer) {
-    return {
-      display: 'block',
-      paddingRight: `${props.theme.space[1]}px`,
-      paddingBottom: `${props.theme.space[2]}px`,
-      fontSize: `${props.theme.fontSizes[1]}px`,
-      letterSpacing: `${props.theme.letterSpacings.text}`,
-      lineHeight: '1.14',
-    };
-  }
-  return null;
-};
-
 const ListItem = styled.li`
   ${alignItems}
   ${border}
@@ -40,7 +25,6 @@ const ListItem = styled.li`
   ${order}
   ${space}
   ${width}
-  ${footer}
 `;
 
 export default ListItem;
