fcbfd674003c4d094e7921e241e38a25e369ef62
Kameshwaran <kamesh@codebrahma.com>
2018-08-03 02:05:55 +0530

    Fixed button, heading, flex and added secondary button component

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,5 +1,6 @@
 export { default as Box } from './Box';
 export { default as Button } from './Button';
+export { default as SecondaryButton } from './SecondaryButton';
 export { default as Image } from './Image';
 export { default as Flex } from './Flex';
 export { default as Text } from './Text';
