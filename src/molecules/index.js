e0d2231932146684b1f4e6a079c22ed546466c3a
Kameshwaran <kamesh@codebrahma.com>
2018-08-02 17:17:58 +0530

    Added container component

--- src/molecules/index.js
+++ src/molecules/index.js
@@ -3,3 +3,4 @@ export { default as ImageCard } from './ImageCard';
 export { default as Testimonial } from './Testimonial';
 export { default as P } from './Typography';
 export { default as TextWithIcon } from './TextWithIcon';
+export { default as Container } from './Container';
