9f98395bb3c475522d5c77eb5b45ee9efbe3c1e3
Anand Narayan <anand@codebrahma.com>
2018-08-06 16:04:08 -0700

    fixed the button

--- src/atoms/Button/index.js
+++ src/atoms/Button/index.js
@@ -11,38 +11,20 @@ import {
   minWidth,
 } from 'styled-system';
 
-import { hexToRgbA } from '../../utils';
 
-
-// TODO refactor the hover state implementation
-// i.e the logic for disabled buttons not to hover is repeated
-// across primary and secondary
-const secondary = (props) => {
-  if (!props.secondary) return null;
-  return {
-    background: 'transparent',
-    border: `1px solid ${props.theme.colors.primaryColor}`,
-    color: `${props.theme.colors.primaryColor}`,
-    '&:hover': {
-      // TODO fix the hover to base on the variable
-      background: props.disabled ? null :
-        hexToRgbA(props.theme.colors.secondaryColor, '0.1'),
-    },
-  };
-};
-
-// TODO: Customize hovering color based on props. This will break the header / hero section button hover
+// TODO: Customize hovering color based on props. 
+// This will break the header / hero section button hover
 const Button = styled.button`
-  ${secondary}
   ${color} ${border} ${borderColor}
   ${fontFamily} ${fontSize} ${space}
   ${letterSpacing} ${width} ${minWidth}
-  transition: background 0.5s ease;
   &:disabled {
     opacity: 0.2;
   }
   &:hover {
-    background-color: ${props => props.disabled ? null : hexToRgbA(props.theme.colors.white, '0.2')};
+    background-color: ${ props => 
+        props.disabled ? null : props.theme.colors.secondaryColor
+      };
     cursor: pointer;
   }
 `;
@@ -50,6 +32,7 @@ const Button = styled.button`
 Button.defaultProps = {
   color: 'white',
   bg: 'primaryColor',
+  border: 'none',
   fontFamily: 'Serverless',
   px: '3.5em',
   py: '1em',
