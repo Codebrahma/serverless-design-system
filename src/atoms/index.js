440b05b47096eb620e249ef361d48d4c7d438b4f
prasanna1211 <prasanna@codebrahma.com>
2018-08-07 20:56:19 +0530

    Updated multiple version of box

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,8 +1,8 @@
 export { default as Box } from './Box';
+export { default as Flex } from './Flex';
 export { default as Button } from './Button';
 export { default as SecondaryButton } from './SecondaryButton';
 export { default as Image } from './Image';
-export { default as Flex } from './Flex';
 export { default as Text } from './Text';
 export { default as TextField } from './TextField';
 export { default as Logo } from './Logo';
