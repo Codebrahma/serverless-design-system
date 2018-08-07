a86f9b051f73d14c31cbdd4c319b9001cfc804c9
prasanna1211 <prasanna@codebrahma.com>
2018-08-07 17:08:11 +0530

    added and removed heading related docs

---
+++ doc/src/content/atoms/comp/List.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import LiveEdit from './src/LiveEdit';
+
+// Line height is not consistent
+const text = `
+  <Flex>
+    <Box>framework</Box>
+    <Box>dashboard</Box>
+    <Box>event gateway</Box>
+    <Box>enterprise</Box>
+  </Flex>
+`;
+
+export default () => (
+  <div>
+    <LiveEdit code={text} />
+    props are
+    <ul>
+      <li>color: string</li>
+      <li>fontFamily: string</li>
+      <li>fontSize: number</li>
+      <li>fontStyle: string</li>
+      <li>fontWeight: string</li>
+      <li>letterSpacing: number</li>
+      <li>lineHeight: number</li>
+    </ul>
+  </div>
+);
