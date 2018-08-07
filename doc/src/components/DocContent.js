64fec656361f9f2fe0304c9967086592efe0355f
prasanna1211 <prasanna@codebrahma.com>
2018-08-07 22:47:45 +0530

    Added all variations for box component

--- doc/src/components/DocContent.js
+++ doc/src/components/DocContent.js
@@ -52,11 +52,14 @@ const DocContent = ({ nodes, scope }) => {
   if (nodes.length === 0) return null;
   const sortedNodes = nodes.sort((a, b) => a.node.frontmatter.path > b.node.frontmatter.path);
   return (
-    <div>
+    <div style={{ fontSize: '16px' }}>
       {
         sortedNodes.map((item, index) => {
           return (item.node.frontmatter.path.split('/')[2]) === 'props' ? (
-            <div key={index} dangerouslySetInnerHTML={{ __html: item.node.html }} />
+            <div>
+              <h3>Props</h3>
+              <div key={index} dangerouslySetInnerHTML={{ __html: item.node.html }} />
+            </div>
           ) : (
             <NodeList key={index} node={item.node} scope={scope} />
           );
