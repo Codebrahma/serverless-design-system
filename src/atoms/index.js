804ba703eb95b2e60f8d11c9cc9a53571cfb7397
prasanna1211 <prasanna@codebrahma.com>
2018-08-08 00:10:55 +0530

    Added inline-block component

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -1,5 +1,6 @@
 export { default as Box } from './Box';
 export { default as Flex } from './Flex';
+export { default as InlineBlock } from './InlineBlock';
 export { default as Button } from './Button';
 export { default as SecondaryButton } from './SecondaryButton';
 export { default as Image } from './Image';
