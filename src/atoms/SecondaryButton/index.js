277835ebe7de63a1b63d7f4163fd36d1f4d5afe9
Kameshwaran <kamesh@codebrahma.com>
2018-08-08 21:36:48 +0530

    Made secondary button as customizable

--- src/atoms/SecondaryButton/index.js
+++ src/atoms/SecondaryButton/index.js
@@ -5,11 +5,17 @@ import { hexToRgbA } from '../../utils';
 
 const SecondaryButton = styled(Button)`
   background: transparent;
-  border: 1px solid ${({ theme }) => theme.colors.primaryColor};
+  border-style: solid;
+  border-width: ${({ borderWidth }) => borderWidth};
+  border-color: ${({ theme }) => theme.colors.primaryColor};
   color: ${({ theme }) => theme.colors.primaryColor};
   &:hover {
     background: ${(props) => (props.disabled ? null : hexToRgbA(props.theme.colors.secondaryColor, '0.2'))};
   };
 `;
+
+SecondaryButton.defaultProps = {
+  borderWidth: '1px',
+};
 
 export default SecondaryButton;
