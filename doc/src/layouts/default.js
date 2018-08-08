c3686c206493b370cdf9dc080d0b22e0fbd4987d
prasanna1211 <prasanna@codebrahma.com>
2018-08-08 11:29:02 +0530

    changed font style

--- doc/src/layouts/default.js
+++ doc/src/layouts/default.js
@@ -3,4 +3,5 @@ import styled from 'styled-components'
 
 export const AppWrapper = styled.div`
   height: 100%;
+  font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
 `;
