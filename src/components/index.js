56ae8cf1ba2929472fca598ee44ac31fc2772875
prasanna1211 <prasanna@codebrahma.com>
2018-08-13 15:02:09 +0530

    Added hero advantage section to components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Testimonial } from './Testimonial';
 export { default as Feature } from './Feature';
 export { default as UseCase } from './UseCase';
+export { default as HeroAdvantage } from './HeroAdvantage';
