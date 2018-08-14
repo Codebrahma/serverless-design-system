a57ffe4918a004ce8603e1ba57836d68f159ca27
prasanna1211 <prasanna@codebrahma.com>
2018-08-15 02:30:45 +0530

    Added dropdown methods

--- src/atoms/DropDown/index.js
+++ src/atoms/DropDown/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import DefaultContainer from './DefaultContainer'
 import DefaultFieldContainer from './DefaultFieldContainer'
@@ -7,7 +8,89 @@ import DefaultIcon from './DefaultIcon'
 import DefaultMenuContainer from './DefaultMenuContainer'
 import DefaultOption from './DefaultOption'
 
+const DEFAULT_PLACEHOLDER_STRING = 'Select...'
+
 class Dropdown extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: props.value || {
+        label: typeof props.placeholder === 'undefined' ? DEFAULT_PLACEHOLDER_STRING : props.placeholder,
+        value: ''
+      },
+      isOpen: false
+    }
+    this.mounted = true
+    this.handleDocumentClick = this.handleDocumentClick.bind(this)
+    this.fireChangeEvent = this.fireChangeEvent.bind(this)
+    this.setValue = this.setValue.bind(this)
+  }
+
+  componentWillReceiveProps (newProps) {
+    if (newProps.value && newProps.value !== this.state.selected) {
+      this.setState({selected: newProps.value})
+    } else if (!newProps.value) {
+      this.setState({selected: {
+        label: typeof newProps.placeholder === 'undefined' ? DEFAULT_PLACEHOLDER_STRING : newProps.placeholder,
+        value: ''
+      }})
+    }
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', this.handleDocumentClick, false)
+    document.addEventListener('touchend', this.handleDocumentClick, false)
+  }
+
+  componentWillUnmount () {
+    this.mounted = false
+    document.removeEventListener('click', this.handleDocumentClick, false)
+    document.removeEventListener('touchend', this.handleDocumentClick, false)
+  }
+
+  handleMouseDown (event) {
+    if (this.props.onFocus && typeof this.props.onFocus === 'function') {
+      this.props.onFocus(this.state.isOpen)
+    }
+    if (event.type === 'mousedown' && event.button !== 0) return
+    event.stopPropagation()
+    event.preventDefault()
+
+    if (!this.props.disabled) {
+      this.setState({
+        isOpen: !this.state.isOpen
+      })
+    }
+  }
+
+  handleDocumentClick (event) {
+    if (this.mounted) {
+      if (!ReactDOM.findDOMNode(this).contains(event.target)) {
+        if (this.state.isOpen) {
+          this.setState({ isOpen: false })
+        }
+      }
+    }
+  }
+
+  setValue (value, label) {
+    let newState = {
+      selected: {
+        value,
+        label
+      },
+      isOpen: false
+    }
+    this.fireChangeEvent(newState)
+    this.setState(newState)
+  }
+
+  fireChangeEvent (newState) {
+    if (newState.selected !== this.state.selected && this.props.onChange) {
+      this.props.onChange(newState.selected)
+    }
+  }
+
   render() {
     return (
       <DefaultContainer>
@@ -20,7 +103,8 @@ class Dropdown extends React.Component {
             this.props.options.map((option) => (
               <DefaultOption
                 {...option}
-                selected={false}
+                selected={this.state.selected}
+                onSelect={this.setValue}
               />
             ))
           }
