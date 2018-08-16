af467abd9e719676f7e62d75793abf6737662db5
Anand Narayan <anand@codebrahma.com>
2018-08-16 13:01:35 -0700

    Removed wrapper and backgroundImage

--- src/molecules/index.js
+++ src/molecules/index.js
@@ -3,4 +3,3 @@ export { default as P } from './Typography';
 export { default as TextWithIcon } from './TextWithIcon';
 export { default as Container } from './Container';
 export { default as ImageList } from './ImageList';
-export { default as Wrapper } from './Wrapper';
