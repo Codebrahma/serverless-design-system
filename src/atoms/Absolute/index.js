0450bad9996b6e44d4358c36d7df4b33bbc01f9d
Anand Narayan <anand@codebrahma.com>
2018-08-17 11:57:58 -0700

    fixed relative and absolute comp paths and lint

--- src/atoms/Absolute/index.js
+++ src/atoms/Absolute/index.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
-import { top, left, right, bottom, zIndex } from 'styled-system'
+import {
+  top,
+  left,
+  right,
+  bottom,
+  zIndex,
+} from 'styled-system';
 
-import Box from './../box';
+import Box from '../Box';
 
 export default styled(Box)`
   position: absolute;
