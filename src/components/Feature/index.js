93dcedb7ac8534789505908be721f870d89ddb8e
prasanna1211 <prasanna@codebrahma.com>
2018-08-19 23:01:32 +0530

    Replaced Box.relative with Relative

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Box, Heading, Image } from '../../atoms';
+import { Heading, Image, Relative } from '../../atoms';
 import { P } from '../../molecules';
 import withBeforeAfter from '../../withBeforeAfter';
 
