4ffd315cbdd1449b7f69e7a0b86fa957ce28b8dc
prasanna1211 <prasanna@codebrahma.com>
2018-08-21 19:16:26 +0530

    Refactored imagelist component with latest atoms

--- src/molecules/ImageList/index.js
+++ src/molecules/ImageList/index.js
@@ -1,19 +1,25 @@
 import React from 'react'
 
-import { Image, ResponsiveStack } from '../../atoms'
+import { Flex, Image, InlineBlock, ResponsiveStack } from '../../atoms'
 
 const EachImage = ({ src: logo, altText, maxWidth, maxHeight }) => (
-  <Image
-    src={logo}
-    alt={altText}
-    maxHeight={maxHeight}
-    maxWidth={maxWidth}
-    my={2}
-  />
+  <InlineBlock my={2}>
+    <Flex.horizontallyCenter>
+      <Image
+        src={logo}
+        alt={altText}
+        maxHeight={maxHeight}
+        maxWidth={maxWidth}
+      />
+    </Flex.horizontallyCenter>
+  </InlineBlock>
 )
 
 const ImageList = ({ list, ...props}) => (
-  <ResponsiveStack.verticallyCenter justifyContent='space-between' width={1}>
+  <ResponsiveStack.verticallyCenter
+    justifyContent='space-between'
+    width={1}
+  >
     {
       list.map(({ src, altText }) => (
         <EachImage key={src} src={src} altText={altText} {...props} />
