4024866b5afc0a89c736233b8c975b1bfcce0968
Kameshwaran <kamesh@codebrahma.com>
2018-08-24 21:18:50 +0530

    Added min height and min width

--- src/atoms/Background/index.js
+++ src/atoms/Background/index.js
@@ -7,6 +7,8 @@ import {
   backgroundRepeat,
   backgroundSize,
   backgroundPosition,
+  minHeight,
+  minWidth,
 } from 'styled-system';
 
 const Background = styled.div`
@@ -17,6 +19,8 @@ const Background = styled.div`
   ${backgroundRepeat}
   ${backgroundSize}
   ${backgroundPosition}
+  ${minHeight}
+  ${minWidth}
 `;
 
 Background.displayName = 'Background';
