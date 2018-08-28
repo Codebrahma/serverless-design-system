56a5b2bd61fe1a3dde97d72b46bb16c52aa7d192
prasanna1211 <prasanna@codebrahma.com>
2018-08-28 12:53:50 +0530

    Added minHeight ad minWidth

--- src/atoms/Image/index.js
+++ src/atoms/Image/index.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
-import { width, height, maxHeight, maxWidth } from 'styled-system';
+import { width, height, maxHeight, maxWidth, minHeight, minWidth } from 'styled-system';
 
 export default styled.img`
   ${width}
   ${height}
   ${maxHeight}
   ${maxWidth}
+  ${minHeight}
+  ${minWidth}
 `;
