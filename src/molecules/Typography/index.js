150f102b64bb6b2c14ea36ace5d9cd39cd002e93
Kameshwaran <kamesh@codebrahma.com>
2018-08-30 12:36:15 +0530

    Added font size and style to paragraph

--- src/molecules/Typography/index.js
+++ src/molecules/Typography/index.js
@@ -5,6 +5,8 @@ const P = Text.withComponent('p');
 
 P.defaultProps = {
   fontFamily: theme.font.p,
+  fontWeight: 'normal',
+  fontStyle: 'normal',
   color: 'gray.3',
   fontSize: 2,
   lineHeight: 4,
