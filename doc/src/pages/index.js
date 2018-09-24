afa3636b620def7813566a1b0ab682355dbde0c4
Kameshwaran <kamesh@codebrahma.com>
2018-09-24 16:58:40 +0530

    Added version

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -1,5 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+  margin-top: 25px;
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  span {
+    font-size: 1.6rem;
+  }
+`
 
 export default () => (
-  <div />
+  <AppWrapper>
+    <h1>
+      Serverless design system <span><code>v1.0</code></span>
+    </h1>
+  </AppWrapper>
 )
