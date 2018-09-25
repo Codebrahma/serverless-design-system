95fbe3c70b0469445b90a54c17d9289092ea48d4
Kameshwaran <kamesh@codebrahma.com>
2018-09-25 17:03:23 +0530

    Updated missing dependency and version

--- dist/themeProvider.js
+++ dist/themeProvider.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
 var _styledComponents = require('styled-components');
 
 var _theme = require('./theme');
@@ -12,7 +16,7 @@ var _theme2 = _interopRequireDefault(_theme);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const CustomThemeProvider = props => React.createElement(_styledComponents.ThemeProvider, props);
+const CustomThemeProvider = props => _react2.default.createElement(_styledComponents.ThemeProvider, props);
 
 CustomThemeProvider.defaultProps = { theme: _theme2.default };
 
